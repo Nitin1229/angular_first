ea9f6a66cf9e8411ffff193a4b77fb613bb38d30
Nitin1229 <nitin.gupta1229@gmail.com>
2021-08-26 22:48:22 +0530

    my first assignment

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'nitin';
+  arr=['Line one', 'line second', 'line third', 'line fourth'];
+  flag=true;
+}
